1b96c51d7790437640e16e5f049e971545cf09e4
amin_ruhul <ruhulinfo50@gmail.com>
2021-07-30 23:21:47 +0600

    set up routes

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.port || 5000;
 
+app.use("/api/users", require("./router/usersRouter"));
+app.use("/api/auth", require("./router/authRouter"));
+app.use("/api/contracts", require("./router/contractsRouter"));
+
 app.listen(PORT, () => {
   console.log(`server running at port: ${PORT}`);
 });
