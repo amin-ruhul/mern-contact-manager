fb1047c1a7634c6e41443e2fbd315efa79190413
amin_ruhul <ruhulinfo50@gmail.com>
2021-07-31 22:05:17 +0600

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,33 @@
 import React from "react";
 import styles from "../../assets/css/Navbar.module.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <h1>Hello</h1>
+      <div className="logo">CM</div>
+      <ul className={styles.navLinks}>
+        <li className={styles.navLink}>
+          <Link className={styles.link} to="/">
+            Home
+          </Link>
+        </li>
+        <li className={styles.navLink}>
+          <Link className={styles.link} to="/about">
+            About
+          </Link>
+        </li>
+        <li className={styles.navLink}>
+          <Link className={styles.link} to="/login">
+            Login
+          </Link>
+        </li>
+        <li className={styles.navLink}>
+          <Link className={styles.link} to="/register">
+            Register
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
