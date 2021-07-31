006b062a15631019ad9b810a8887d560223c8a7c
amin_ruhul <ruhulinfo50@gmail.com>
2021-07-31 20:47:50 +0600

    add client

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../assets/css/Navbar.module.css";
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default Navbar;
