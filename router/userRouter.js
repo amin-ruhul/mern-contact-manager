aac91f1c4be738d02dec9900e47a16555c346e63
amin_ruhul <ruhulinfo50@gmail.com>
2021-07-31 12:41:23 +0600

    set up jwt

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../models/User");
@@ -16,23 +18,39 @@ router.post(
   async (req, res) => {
     const error = validationResult(req);
     if (!error.isEmpty()) {
-      res.status(400).json({ error: error.array() });
-    } else {
-      const { name, email, password } = req.body;
-      try {
-        const dbUser = await User.findOne({ email });
-        if (dbUser) {
-          res.status(400).json({ error: "Email already used." });
-        } else {
-          const user = new User({ name, email, password });
-          const hasPassword = await bcrypt.hash(password, 10);
-          user.password = hasPassword;
-          const newUser = await user.save();
-          res.status(200).json({ user: newUser });
-        }
-      } catch (error) {
-        res.status(400).json({ error: error.message });
+      return res.status(400).json({ error: error.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      const dbUser = await User.findOne({ email });
+      if (dbUser) {
+        return res.status(400).json({ error: "Email already used." });
       }
+      const user = new User({ name, email, password });
+      const hasPassword = await bcrypt.hash(password, 10);
+      user.password = hasPassword;
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      res.status(400).json({ error: "Server error" });
+      console.log(error.message);
     }
   }
 );
