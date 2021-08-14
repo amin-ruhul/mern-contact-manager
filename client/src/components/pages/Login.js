66c3079515e10da994f3a48dd7a817ca66700d31
amin_ruhul <ruhulinfo50@gmail.com>
2021-08-14 21:42:13 +0600

    add login page

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = user;
+
+  const handelChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
     <div>
-      <h1>Login</h1>
+      <form onSubmit={handelSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handelChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handelChange}
+        />
+        <input type="submit" value="Login" />
+      </form>
     </div>
   );
 }
