54d56f710a870ccb366f8da684731d4e48cb5824
amin_ruhul <ruhulinfo50@gmail.com>
2021-08-22 10:41:33 +0600

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dbConnection = require("./config/db");
+const path = require("path");
 
 const PORT = process.env.port || 5000;
 
@@ -10,6 +11,13 @@ dbConnection();
 app.use("/api/user", require("./router/userRouter"));
 app.use("/api/auth", require("./router/authRouter"));
 app.use("/api/contacts", require("./router/contactsRouter"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => {
   console.log(`server running at port: ${PORT}`);
